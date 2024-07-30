c624a922708ce5a801ee056e16484b2dd80761b1
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-07-30 15:04:17 +0000

    fix build problems

--- generate_checksum.sh
+++ generate_checksum.sh
@@ -11,15 +11,17 @@ do
     COIN_libwallet2_api_c_date=$(git log -1 --format=%ci -- ${coin}_libwallet2_api_c)
 
     cat > ${coin}_libwallet2_api_c/src/main/cpp/${coin}_checksum.h << EOF
+#ifndef MONEROC_CHECKSUMS
+#define MONEROC_CHECKSUMS
 const char * ${COIN}_wallet2_api_c_h_sha256 = "${COIN_wallet2_api_c_h_sha256}";
 const char * ${COIN}_wallet2_api_c_cpp_sha256 = "${COIN_wallet2_api_c_cpp_sha256}";
 const char * ${COIN}_wallet2_api_c_exp_sha256 = "${COIN_wallet2_api_c_exp_sha256}";
 const int ${COIN}_libwallet2_api_c_version = ${COIN_libwallet2_api_c_version};
 const char * ${COIN}_wallet2_api_c_date = "${COIN_libwallet2_api_c_date}";
+#endif
 EOF
     cat > impls/monero.dart/lib/src/checksum_${coin}.dart << EOF
 // ignore_for_file: constant_identifier_names
-
 const String wallet2_api_c_h_sha256 = "${COIN_wallet2_api_c_h_sha256}";
 const String wallet2_api_c_cpp_sha256 = "${COIN_wallet2_api_c_cpp_sha256}";
 const String wallet2_api_c_exp_sha256 = "${COIN_wallet2_api_c_exp_sha256}";
