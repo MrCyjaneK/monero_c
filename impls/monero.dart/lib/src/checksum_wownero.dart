0737e1a7a1335b77b9bd90505241d5647eb7fd3e
cyan <cyjan@mrcyjanek.net>
2024-07-30 20:22:25 +0200

    Fix darwin builds (#24)
    * Update wownero_libwallet2_api_c.exp

--- impls/monero.dart/lib/src/checksum_wownero.dart
+++ impls/monero.dart/lib/src/checksum_wownero.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 const String wallet2_api_c_h_sha256 = "8a8d386dd5d996c89a0586c55b295ef95ca584bf1ffa26255152b291910a0a77";
 const String wallet2_api_c_cpp_sha256 = "ed400bd9c4709383ffd42a9fbe68be37a2a47a42f92eacaf3a2dbd248c422739";
-const String wallet2_api_c_exp_sha256 = "dd9874cc43a6a74bbfa3e49c4ad3f835ff22efd3de8f679cc91c4af3d931aedb";
+const String wallet2_api_c_exp_sha256 = "3673e40e1a7115552276d1d541f6e4d5a0fef47c40fff7b988f49923af84c8a4";
