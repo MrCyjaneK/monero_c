e3b88da039d4790cd6624bdab32801ccd6d7b9e6
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-07-30 15:13:50 +0000

    update checksums

--- wownero_libwallet2_api_c/src/main/cpp/wownero_checksum.h
+++ wownero_libwallet2_api_c/src/main/cpp/wownero_checksum.h
@@ -3,6 +3,6 @@
 const char * WOWNERO_wallet2_api_c_h_sha256 = "1f4688b9ddacbc33e82426c50480ab8e3727718f59e577080a7dc6dca48bcf9d";
 const char * WOWNERO_wallet2_api_c_cpp_sha256 = "f2b818197f90711d938c17ce0513b110b1e2b4fe9765f54be7afa5a3b4ad8077";
 const char * WOWNERO_wallet2_api_c_exp_sha256 = "dd9874cc43a6a74bbfa3e49c4ad3f835ff22efd3de8f679cc91c4af3d931aedb";
-const int WOWNERO_libwallet2_api_c_version = 23;
-const char * WOWNERO_wallet2_api_c_date = "2024-07-30 15:12:07 +0000";
+const int WOWNERO_libwallet2_api_c_version = 25;
+const char * WOWNERO_wallet2_api_c_date = "2024-07-30 15:13:50 +0000";
 #endif
