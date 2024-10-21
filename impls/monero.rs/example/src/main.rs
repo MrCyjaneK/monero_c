06beb39b2e62c585b29fa5b435ee645dbfa1bca5
sneurlax <sneurlax@gmail.com>
2024-10-20 20:44:03 -0500

    update example

--- impls/monero.rs/example/src/main.rs
+++ impls/monero.rs/example/src/main.rs
@@ -1,26 +1,32 @@
-use monero_rust::{network, WalletError, WalletManager};
+use monero_c_rust::{NetworkType, WalletError, WalletManager};
+use tempfile::TempDir;
 
 fn main() -> Result<(), WalletError> {
-    let wallet_manager = WalletManager::new(None)?;
+    let manager = WalletManager::new()?;
 
-    let wallet = wallet_manager.create_wallet(
-        "wallet_name",
-        "password",
-        "English",
-        network::MAINNET,
-    )?;
+    let temp_dir = TempDir::new().expect("Failed to create temporary directory");
+    let wallet_path = temp_dir.path().join("test_wallet");
+    let wallet_str = wallet_path.to_str().unwrap();
+
+    let wallet = manager.restore_polyseed(
+        wallet_str.to_string(),
+        "password".to_string(),
+        "capital chief route liar question fix clutch water outside pave hamster occur always learn license knife".to_string(),
+        NetworkType::Mainnet,
+        0, // Restore from the beginning of the blockchain.
+        1, // Default KDF rounds.
+        "".to_string(), // No seed offset.
+        true, // Create a new wallet.
+    );
 
     println!("Wallet created successfully.");
 
-    match wallet.get_seed("") {
-        Ok(seed) => println!("Seed: {}", seed),
-        Err(e) => eprintln!("Failed to get seed: {:?}", e),
-    }
+    // Print the primary address.
+    println!("Primary address: {}", wallet?.get_address(0, 0)?);
 
-    match wallet.get_address(0, 0) {
-        Ok(address) => println!("Primary address: {}", address),
-        Err(e) => eprintln!("Failed to get address: {:?}", e),
-    }
+    // Clean up the wallet.
+    std::fs::remove_file(wallet_str).expect("Failed to delete test wallet");
+    std::fs::remove_file(format!("{}.keys", wallet_str)).expect("Failed to delete test wallet keys");
 
     Ok(())
 }
