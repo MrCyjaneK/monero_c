da2c47eb09ee6e15f117ba267711ec176cb2bcc3
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-10-31 13:15:13 +0000

    update checksum, fix zano patches on CI

--- impls/monero.dart/lib/src/checksum_zano.dart
+++ impls/monero.dart/lib/src/checksum_zano.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 const String wallet2_api_c_h_sha256 = "e37ae91020ddb248bcc206678ef9a8e2754f71d498ee0b09a00e5ad070a4d8a7";
-const String wallet2_api_c_cpp_sha256 = "bbcd07c986d29b482a2b3fcbab78f12ecfdce096b8178945fdec0d5b600b315a-";
-const String wallet2_api_c_exp_sha256 = "c8913ac41068f67b57c9b0a3c7dd8973e3c1273b66c2ff0aadb0003931da748c";
+const String wallet2_api_c_cpp_sha256 = "bbcd07c986d29b482a2b3fcbab78f12ecfdce096b8178945fdec0d5b600b315a-59ea01339d958144736b614ab7d5f5e00fdf837b";
+const String wallet2_api_c_exp_sha256 = "78c7d24804c6f5c900ddf901ea101192da356af2e29f83c20226cc291ec9875b";
--- impls/monero.ts/checksum_zano.ts
+++ impls/monero.ts/checksum_zano.ts
@@ -1,5 +1,5 @@
 export const zanoChecksum = {
     wallet2_api_c_h_sha256: "e37ae91020ddb248bcc206678ef9a8e2754f71d498ee0b09a00e5ad070a4d8a7",
-    wallet2_api_c_cpp_sha256: "bbcd07c986d29b482a2b3fcbab78f12ecfdce096b8178945fdec0d5b600b315a-",
-    wallet2_api_c_exp_sha256: "c8913ac41068f67b57c9b0a3c7dd8973e3c1273b66c2ff0aadb0003931da748c",
+    wallet2_api_c_cpp_sha256: "bbcd07c986d29b482a2b3fcbab78f12ecfdce096b8178945fdec0d5b600b315a-59ea01339d958144736b614ab7d5f5e00fdf837b",
+    wallet2_api_c_exp_sha256: "78c7d24804c6f5c900ddf901ea101192da356af2e29f83c20226cc291ec9875b",
 }
