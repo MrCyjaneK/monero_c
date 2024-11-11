d47fb528282fd795ec33da79b5bd218840495629
Konstantin Ullrich <konstantinullrich12@gmail.com>
2024-11-11 16:55:28 +0100

    Remove ledger_flutter_plus dependency

--- monero_libwallet2_api_c/src/main/cpp/monero_checksum.h
+++ monero_libwallet2_api_c/src/main/cpp/monero_checksum.h
@@ -2,5 +2,5 @@
 #define MONEROC_CHECKSUMS
 const char * MONERO_wallet2_api_c_h_sha256 = "198a27486ce4f014b737f52ec2879a90838c3b3ca088de8dc7d55b79a10f4a5b";
 const char * MONERO_wallet2_api_c_cpp_sha256 = "22b0c9dd542fb55edfe9e17da67e97ddf5f80bcdd3e126e1bd64cce54234bde1-b089f9ee69924882c5d14dd1a6991deb05d9d1cd";
-const char * MONERO_wallet2_api_c_exp_sha256 = "c8913ac41068f67b57c9b0a3c7dd8973e3c1273b66c2ff0aadb0003931da748c";
+const char * MONERO_wallet2_api_c_exp_sha256 = "c1f785d62709fd8b849063ecf4fe8854d9e3b05b568b9716de98d33e7bdaf522";
 #endif
