fffd22288aa9384add744a04251f68ec65124980
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-11-28 08:38:54 -0500

    Cleanup patches

--- impls/monero.dart/lib/src/checksum_wownero.dart
+++ impls/monero.dart/lib/src/checksum_wownero.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 const String wallet2_api_c_h_sha256 = "8a8d386dd5d996c89a0586c55b295ef95ca584bf1ffa26255152b291910a0a77";
-const String wallet2_api_c_cpp_sha256 = "07d67f34a07869aaa4af6ca04e142dbad2fb1fba0e2ebdefd22bc333fd982e25-dd46a31f3cab67b316e9239b15acf7d5cea60aa9";
+const String wallet2_api_c_cpp_sha256 = "80ec887a70b5198968a402cba0aca65880b55277ea6b1af718efa3951814a6cf-dd46a31f3cab67b316e9239b15acf7d5cea60aa9";
 const String wallet2_api_c_exp_sha256 = "3673e40e1a7115552276d1d541f6e4d5a0fef47c40fff7b988f49923af84c8a4";
