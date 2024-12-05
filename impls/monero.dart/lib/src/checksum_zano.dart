d70685a2deab5ae38d70f99c6a57a25475062e43
Czarek Nakamoto <cyjan@mrcyjanek.net>
2024-12-05 09:41:21 -0600

    update zano build

--- impls/monero.dart/lib/src/checksum_zano.dart
+++ impls/monero.dart/lib/src/checksum_zano.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 const String wallet2_api_c_h_sha256 = "8acaa95513b85a984c08e05cc3f2ac7530bb8f32946eeeb45357bd846aef33dd";
-const String wallet2_api_c_cpp_sha256 = "258e315ccc5f5ffa6d3d6608e9bfd7ac4f3b91b36424ad1633ed4440bf2986bc-59ea01339d958144736b614ab7d5f5e00fdf837b";
+const String wallet2_api_c_cpp_sha256 = "94c3f49fe8a8acd9693c08712b9c38ecdb3f1bfed0a22b6c3462228ed64b82ba-2817090c8ac7639d6f697d00fc8bcba2b3681d90";
 const String wallet2_api_c_exp_sha256 = "66f3ff655bbfd11ad28c318ab707090b5a93276f436b06f7b1c0f329dba3c9c2";
