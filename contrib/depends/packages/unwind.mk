ec38214262fb24acf07d32f0b1d442cc71e4059e
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-02-04 17:18:05 +0100

    one more

--- contrib/depends/packages/unwind.mk
+++ contrib/depends/packages/unwind.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash=90337653d92d4a13de590781371c604f9031cdb50520366aa1e3a91e1
 $(package)_patches=fix_obj_order.patch
 
 define $(package)_preprocess_cmds
-  patch -p0 < $($(package)_patch_dir)/fix_obj_order.patch
+  patch -p0 < $($(package)_patch_dir)/fix_obj_order.patch &&\
   cp -f $(BASEDIR)/config.guess config/config.guess &&\
   cp -f $(BASEDIR)/config.sub config/config.sub
 endef
