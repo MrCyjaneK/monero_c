1585ce94b9ea4668aac5123c52f78400b896c1e4
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-02-04 17:14:36 +0100

    fix syntax error

--- contrib/depends/packages/protobuf.mk
+++ contrib/depends/packages/protobuf.mk
@@ -20,7 +20,7 @@ define $(package)_preprocess_cmds
   cp -f $(BASEDIR)/config.guess third_party/googletest/googletest/build-aux/config.guess &&\
   cp -f $(BASEDIR)/config.sub third_party/googletest/googletest/build-aux/config.sub &&\
   cp -f $(BASEDIR)/config.guess third_party/googletest/googlemock/build-aux/config.guess &&\
-  cp -f $(BASEDIR)/config.sub third_party/googletest/googlemock/build-aux/config.sub &&\
+  cp -f $(BASEDIR)/config.sub third_party/googletest/googlemock/build-aux/config.sub
 endef
 
 define $(package)_config_cmds
