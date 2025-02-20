482aee78f4168550043afac9ceb9c8ea180057a7
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-02-20 14:42:11 +0100

    update checksum

--- impls/monero.ts/checksum_monero.ts
+++ impls/monero.ts/checksum_monero.ts
@@ -1,5 +1,5 @@
 export const moneroChecksum = {
     wallet2_api_c_h_sha256: "9e80c4b59a0509aa02fbf01e8df2881b89f82225d1765bfa7856cbdbaf7af116",
-    wallet2_api_c_cpp_sha256: "d229507db508e574bd2badf4819a38dbead8c16a84311ad32c22c887a6003439-b089f9ee69924882c5d14dd1a6991deb05d9d1cd",
+    wallet2_api_c_cpp_sha256: "d229507db508e574bd2badf4819a38dbead8c16a84311ad32c22c887a6003439-0232839913b13cf0ab0bb7ad25fff0c05f37d2fe",
     wallet2_api_c_exp_sha256: "d0f95f1f3bc49f1f59fe4eb0b61826128d7d3bb75405d5a01a252d02db03097d",
 }
