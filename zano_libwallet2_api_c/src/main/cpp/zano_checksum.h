2e3405fa5729fbbdcc30eac07e83e0f572e178d9
cyan <cyjan@mrcyjanek.net>
2025-04-18 00:10:47 +0200

    sdk updates (#133)
    * cmake: fix minimum required version
    
    * fix: unary_function -> __unary_function
    
    * fix it only where it's broken
    
    * update zano to 400
    
    * improve ci speed, update xcode
    
    * update dockerfile, remove zano mingw
    
    * 16.2

--- zano_libwallet2_api_c/src/main/cpp/zano_checksum.h
+++ zano_libwallet2_api_c/src/main/cpp/zano_checksum.h
@@ -1,6 +1,6 @@
 #ifndef MONEROC_CHECKSUMS
 #define MONEROC_CHECKSUMS
 const char * ZANO_wallet2_api_c_h_sha256 = "8acaa95513b85a984c08e05cc3f2ac7530bb8f32946eeeb45357bd846aef33dd";
-const char * ZANO_wallet2_api_c_cpp_sha256 = "4efacd3812d53dd268b6869cc0a9560e7320574d96e09136cf067f796edfeba6-ccfc0327e05f73478b8493accec4a7a1878ff4b1";
+const char * ZANO_wallet2_api_c_cpp_sha256 = "4efacd3812d53dd268b6869cc0a9560e7320574d96e09136cf067f796edfeba6-83eefd205e3df84da0bf095257459fe778330316";
 const char * ZANO_wallet2_api_c_exp_sha256 = "66f3ff655bbfd11ad28c318ab707090b5a93276f436b06f7b1c0f329dba3c9c2";
 #endif
