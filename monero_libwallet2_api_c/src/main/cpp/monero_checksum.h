f415fc58d69ad5ba53b7d613c21fc7d6d8fc219d
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-05-25 17:27:54 +0200

    feat: improve scanning speed

--- monero_libwallet2_api_c/src/main/cpp/monero_checksum.h
+++ monero_libwallet2_api_c/src/main/cpp/monero_checksum.h
@@ -2,5 +2,5 @@
 #define MONEROC_CHECKSUMS
 const char * MONERO_wallet2_api_c_h_sha256 = "91218f60e937753a606b1db899ee2ed66a1b02144eb2d79b950ce59804397f84";
 const char * MONERO_wallet2_api_c_cpp_sha256 = "282128494f3419c128dff7ed67daadd9c2ad0600d10557e829df03d9bc1ad891-0232839913b13cf0ab0bb7ad25fff0c05f37d2fe";
-const char * MONERO_wallet2_api_c_exp_sha256 = "d0f95f1f3bc49f1f59fe4eb0b61826128d7d3bb75405d5a01a252d02db03097d";
+const char * MONERO_wallet2_api_c_exp_sha256 = "b2f2355242f9ad852ccf860a60a5c7a74054f9450ba1fbf9c0f42c58dbfdee9d";
 #endif
