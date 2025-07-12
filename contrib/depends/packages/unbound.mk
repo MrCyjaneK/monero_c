5a6680038375248f6e886a89953439515c048c54
Czarek Nakamoto <cyjan@mrcyjanek.net>
2025-07-12 21:16:57 +0200

    feat: bump android_ndk to r28 chore: bump unbound to fix build issues

--- contrib/depends/packages/unbound.mk
+++ contrib/depends/packages/unbound.mk
@@ -1,12 +1,12 @@
 package=unbound
-$(package)_version=1.19.1
+$(package)_version=1.23.0
 $(package)_download_path=https://www.nlnetlabs.nl/downloads/$(package)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=bc1d576f3dd846a0739adc41ffaa702404c6767d2b6082deb9f2f97cbb24a3a9
+$(package)_sha256_hash=959bd5f3875316d7b3f67ee237a56de5565f5b35fc9b5fc3cea6cfe735a03bb8
 $(package)_dependencies=openssl expat
 $(package)_patches=disable-glibc-reallocarray.patch
 
-
+# ac_cv_type_pthread_spinlock_t -> disappeared in ndk r28
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --enable-static --without-pyunbound --prefix=$(host_prefix)
   $(package)_config_opts+=--with-libexpat=$(host_prefix) --with-ssl=$(host_prefix) --with-libevent=no
@@ -14,6 +14,7 @@ define $(package)_set_vars
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_w64=--enable-static-exe --sysconfdir=/etc --prefix=$(host_prefix) --target=$(host_prefix)
   $(package)_config_opts_x86_64_darwin=ac_cv_func_SHA384_Init=yes
+  $(package)_config_opts_android=ac_cv_type_pthread_spinlock_t=no
   $(package)_build_opts_mingw32=LDFLAGS="$($(package)_ldflags) -lpthread"
   $(package)_cflags_mingw32+="-D_WIN32_WINNT=0x600"
 endef
@@ -24,7 +25,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf) ac_cv_func_getentropy=no
+  $($(package)_autoconf) $($(package)_config_opts) ac_cv_func_getentropy=no
 endef
 
 define $(package)_build_cmds
