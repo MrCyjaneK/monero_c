e7644449a957269f69ccdae0d7c91ab96f0a8831
Czarek Nakamoto <cyjan@mrcyjanek.net>
2026-03-09 16:46:21 +0100

    AAWASTREYDRFUGTIHYJHGUTYFRDTFYVGUBHINJHGTYFRDSRTXDTCFHBJ

--- builder/release.sh
+++ builder/release.sh
@@ -9,5 +9,5 @@ docker build -t $img -f ./builder/Dockerfile .
 
 docker create --name temp_extract $img /bin/sh -c 'sleep 3000'
 
-docker cp temp_extract:/w/* ./release/$(git describe --tags)
+docker cp temp_extract:/release ./release/$(git describe --tags)
 docker rm temp_extract || true
