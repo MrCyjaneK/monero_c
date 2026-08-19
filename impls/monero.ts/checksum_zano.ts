c08045272f0b44a4ebe0d3b26e38f6dd3e84e688
Czarek Nakamoto <cyjan@mrcyjanek.net>
2026-08-19 03:55:59 -0400

    bump: simplybs

--- impls/monero.ts/checksum_zano.ts
+++ impls/monero.ts/checksum_zano.ts
@@ -1,5 +1,5 @@
 export const zanoChecksum = {
     wallet2_api_c_h_sha256: "8acaa95513b85a984c08e05cc3f2ac7530bb8f32946eeeb45357bd846aef33dd",
-    wallet2_api_c_cpp_sha256: "cdec2578df387ab3587712f77470500934d7f18c27201ae71bc3ee52cb784cbd-b372a99c7ada82caac53b677d9aaa7c0902e3b0b",
+    wallet2_api_c_cpp_sha256: "cdec2578df387ab3587712f77470500934d7f18c27201ae71bc3ee52cb784cbd-b76fa185850a9134ecf207bca74c0815c3d49838",
     wallet2_api_c_exp_sha256: "66f3ff655bbfd11ad28c318ab707090b5a93276f436b06f7b1c0f329dba3c9c2",
 }
